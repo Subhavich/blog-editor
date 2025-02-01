97962acc192c8db660f3e45f1bb14b32319be017
Subha Sintaveevonge <100717747+Subhavich@users.noreply.github.com>
2025-02-01 17:13:22 +0700

    export EditorForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import { useReducer, useRef, useState, useEffect } from "react";
-import { alignmentOptions, bgOptions, screenOptions } from "./Data";
+import { screenOptions } from "./Data";
 import { options } from "./components/blocks/BlockOutlet";
-import {
-  AiFillCaretDown,
-  AiFillCaretUp,
-  AiOutlineDelete,
-} from "react-icons/ai";
-import DynamicInput from "./components/DynamicInput";
+import EditorForm from "./components/EditorForm";
 import Result from "./components/Results";
 // Reducer function to manage the editors state
 function reducer(state, action) {
@@ -108,7 +103,7 @@ function App() {
         {isSidebarOpen && (
           <div className="pl-2 pr-8">
             <p className="text-center block py-2 text-lg">EDITORs</p>
-
+            {/* Editor Form Map */}
             <main className="space-y-4 mb-4">
               {editors.map((editor, index) => (
                 <EditorForm
@@ -179,117 +174,3 @@ function App() {
 }
 
 export default App;
-
-// Component that renders dynamic input fields based on load keys
-function EditorForm({ index, type, load, dispatch, expanded }) {
-  const handleConfigChange = (e, arg) => {
-    dispatch({
-      type: "UPDATE_EDITOR",
-      index,
-      key: "config",
-      value: { ...load.config, [arg]: e.target.value },
-    });
-  };
-
-  return (
-    <div className=" p-2 border rounded shadow-sm">
-      <div
-        className={` ${
-          expanded ? "mb-1" : ""
-        } flex justify-between space-x-4 items-center `}
-      >
-        <button
-          onClick={() => dispatch({ type: "DELETE_EDITOR", index })}
-          className="text-rose-800 transition-all"
-        >
-          <AiOutlineDelete className="cursor-pointer" size={24} />
-        </button>
-
-        {/* Expand/Collapse Title */}
-        <p
-          onClick={() =>
-            dispatch({
-              type: "UPDATE_EDITOR",
-              index,
-              key: "expanded",
-              value: !expanded,
-            })
-          }
-          className="cursor-pointer text-lg font-bold flex-1 text-center"
-        >
-          {type.toUpperCase()}
-        </p>
-
-        {/* up-down btn */}
-        <div className="flex space-x-2">
-          <span
-            className="cursor-pointer text-lg font-bold"
-            onClick={() =>
-              dispatch({
-                type: "REORDER_EDITOR",
-                index,
-                increment: -1,
-              })
-            }
-          >
-            <AiFillCaretUp />
-          </span>
-          <span
-            className="cursor-pointer text-lg font-bold"
-            onClick={() =>
-              dispatch({
-                type: "REORDER_EDITOR",
-                index,
-                increment: 1,
-              })
-            }
-          >
-            <AiFillCaretDown />
-          </span>
-        </div>
-      </div>
-      {expanded && (
-        <>
-          <hr className="mb-2" />
-          <div className="flex flex-col space-y-2">
-            {/* Color/Align */}
-            <div className="flex space-x-2 mt-2">
-              <select
-                className="bg-white p-1"
-                onChange={(e) => handleConfigChange(e, "align")}
-              >
-                {alignmentOptions.map((opt) => (
-                  <option key={opt.value} value={opt.value}>
-                    {opt.label}
-                  </option>
-                ))}
-              </select>
-              <select
-                className="bg-white p-1"
-                onChange={(e) => handleConfigChange(e, "bg")}
-              >
-                {bgOptions.map((opt) => (
-                  <option key={opt.value} value={opt.value}>
-                    {opt.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-            {load &&
-              Object.keys(load).map((key) =>
-                key !== "expanded" ? (
-                  <DynamicInput
-                    key={key}
-                    index={index}
-                    label={key}
-                    value={load[key]}
-                    dispatch={dispatch}
-                  />
-                ) : null
-              )}
-          </div>
-        </>
-      )}
-    </div>
-  );
-}
---
+++ src/components/EditorForm.jsx
@@ -0,0 +1,121 @@
+import {
+  AiFillCaretDown,
+  AiFillCaretUp,
+  AiOutlineDelete,
+} from "react-icons/ai";
+import { alignmentOptions, bgOptions } from "../Data";
+import DynamicInput from "./DynamicInput";
+function EditorForm({ index, type, load, dispatch, expanded }) {
+  const handleConfigChange = (e, arg) => {
+    dispatch({
+      type: "UPDATE_EDITOR",
+      index,
+      key: "config",
+      value: { ...load.config, [arg]: e.target.value },
+    });
+  };
+
+  return (
+    <div className=" p-2 border rounded shadow-sm">
+      <div
+        className={` ${
+          expanded ? "mb-1" : ""
+        } flex justify-between space-x-4 items-center `}
+      >
+        <button
+          onClick={() => dispatch({ type: "DELETE_EDITOR", index })}
+          className="text-rose-800 transition-all"
+        >
+          <AiOutlineDelete className="cursor-pointer" size={24} />
+        </button>
+
+        {/* Expand/Collapse Title */}
+        <p
+          onClick={() =>
+            dispatch({
+              type: "UPDATE_EDITOR",
+              index,
+              key: "expanded",
+              value: !expanded,
+            })
+          }
+          className="cursor-pointer text-lg font-bold flex-1 text-center"
+        >
+          {type.toUpperCase()}
+        </p>
+
+        {/* up-down btn */}
+        <div className="flex space-x-2">
+          <span
+            className="cursor-pointer text-lg font-bold"
+            onClick={() =>
+              dispatch({
+                type: "REORDER_EDITOR",
+                index,
+                increment: -1,
+              })
+            }
+          >
+            <AiFillCaretUp />
+          </span>
+          <span
+            className="cursor-pointer text-lg font-bold"
+            onClick={() =>
+              dispatch({
+                type: "REORDER_EDITOR",
+                index,
+                increment: 1,
+              })
+            }
+          >
+            <AiFillCaretDown />
+          </span>
+        </div>
+      </div>
+      {expanded && (
+        <>
+          <hr className="mb-2" />
+          <div className="flex flex-col space-y-2">
+            {/* Color/Align */}
+            <div className="flex space-x-2 mt-2">
+              <select
+                className="bg-white p-1"
+                onChange={(e) => handleConfigChange(e, "align")}
+              >
+                {alignmentOptions.map((opt) => (
+                  <option key={opt.value} value={opt.value}>
+                    {opt.label}
+                  </option>
+                ))}
+              </select>
+              <select
+                className="bg-white p-1"
+                onChange={(e) => handleConfigChange(e, "bg")}
+              >
+                {bgOptions.map((opt) => (
+                  <option key={opt.value} value={opt.value}>
+                    {opt.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            {load &&
+              Object.keys(load).map((key) =>
+                key !== "expanded" ? (
+                  <DynamicInput
+                    key={key}
+                    index={index}
+                    label={key}
+                    value={load[key]}
+                    dispatch={dispatch}
+                  />
+                ) : null
+              )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default EditorForm;
